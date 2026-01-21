0c813cddfed071fee0c97a5b61d74b0e408242b4
KOLA VARUN KUMAR <133854142+kolavarunkumar@users.noreply.github.com>
2026-01-21 23:52:41 +0530

    Update settings.py

--- voter_backend/settings.py
+++ voter_backend/settings.py
@@ -73,15 +73,6 @@ WSGI_APPLICATION = 'voter_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -145,10 +136,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'voter_db',
-        'USER': 'postgres_user',
-        'PASSWORD': 'password',
-        'HOST': 'your_db_host',
-        'PORT': '5432',
+        'USER': 'root',
+        'PASSWORD': 'Unkcoder1337@',  # or your MySQL password
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
